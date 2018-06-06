8e16f210a2608fa9de054572df49a6051c43a0e1
Panagiotis Matigakis <pmatigakis@gmail.com>
2018-06-06 22:27:25 +0300

    It is possible to define how unauthorized access is handled

--- flask_uauth/decorators.py
+++ flask_uauth/decorators.py
@@ -2,20 +2,26 @@ from functools import wraps
 
 from flask import request, current_app
 from flask import _request_ctx_stack
-from werkzeug.exceptions import Unauthorized
 
 
 def authentication_required(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        authorization_value = request.headers.get("Authorization")
-        if authorization_value is None:
-            raise Unauthorized()
-
         uauth = current_app.extensions["uauth"]
+        authorization_value = None
+
+        if uauth.header is not None:
+            authorization_value = request.headers.get(uauth.header)
+
+        if authorization_value is None and uauth.argument is not None:
+            authorization_value = request.args.get(uauth.argument)
+
+        if authorization_value is None:
+            return uauth._handle_missing_token()
+
         token = uauth.get_token(authorization_value)
         if token is None or not token.is_active():
-            raise Unauthorized()
+            return uauth._handle_unauthorized_user()
 
         _request_ctx_stack.top.current_token = token
 
--- flask_uauth/uauth.py
+++ flask_uauth/uauth.py
@@ -1,14 +1,34 @@
+from werkzeug.exceptions import Unauthorized
+
+
 class UAuth(object):
-    def __init__(self, app=None, authentication_callback=None):
+    def __init__(self, app=None, authentication_callback=None, header=None,
+                 argument=None):
         self.app = app
         self.authentication_callback = authentication_callback
+        self.header = header or "Authorization"
+        self.argument = argument
 
         if app is not None:
-            self.init_app(app, self.authentication_callback)
+            self.init_app(
+                app=app,
+                authentication_callback=self.authentication_callback,
+                header=self.header,
+                argument=self.argument
+            )
 
-    def init_app(self, app, authentication_callback=None):
+    def _handle_unauthorized_user(self):
+        raise Unauthorized()
+
+    def _handle_missing_token(self):
+        raise Unauthorized()
+
+    def init_app(self, app, authentication_callback=None, header=None,
+                 argument=None):
         self.authentication_callback = (
                 authentication_callback or self.authentication_callback)
+        self.header = header or self.header
+        self.argument = argument or self.argument
 
         if not hasattr(app, "extensions"):
             app.extensions = {}
